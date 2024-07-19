38d80212fd640d409a57a0cf9e6aa2780220a15d
aline <alinecarvalho.pro@gmail.com>
2024-07-19 14:25:39 -0300

    feat: icon Button component

---
+++ src/components/Buttons/IconButton/index.tsx
@@ -0,0 +1,16 @@
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+
+interface ButtonProps extends TouchableOpacityProps {
+  icon?: React.ReactNode;
+  onPress?: () => void;
+}
+
+const IconButton = ({icon, onPress}: ButtonProps) => {
+  return (
+    <TouchableOpacity style={{width: 30, height: 30}} onPress={onPress}>
+      {icon}
+    </TouchableOpacity>
+  );
+};
+
+export default IconButton;
--- src/screens/Meditation/StartMeditation/index.tsx
+++ src/screens/Meditation/StartMeditation/index.tsx
@@ -1,7 +1,6 @@
 import {SafeAreaView, Text} from 'react-native';
 
 const StartMeditationScreen = () => {
-
   return (
     <SafeAreaView style={{height: '100%', width: '100%'}}>
       <Text>Tela Iniciar meditação</Text>
